eb07dfb69212d27a15ded61dc2deae28f6ae48ce
Arkadiusz Skubala <arkadiusz.skubala@atos.net>
2021-08-24 12:22:17 +0200

    initial commit

--- src/specs/scopes-and-closures/index.spec.js
+++ src/specs/scopes-and-closures/index.spec.js
@@ -1,37 +1,38 @@
 describe('global scope - scopes and closures', () => {
-    it('should belong to the global scope', function(){
+    test('should belong to the global scope', () => {
         // declare a variable MyNumber of type number, that should be available in the global scope (outside the MyMathLibrary object)
         // update the multiplyByTwo() method code to multiply the value of MyNumber by 2
         // make sure the MyMathLibrary.multiplyByTwo() method has been called
 
-        var MyMathLibrary = {
-            multiplyByTwo: function() {
+        let MyMathLibrary = {
+            multiplyByTwo: () => {
                 /* ... */
+                return;
             }
         }
 
-        spyOn(MyMathLibrary, "multiplyByTwo").and.callThrough();
+        spyOnMultiplyByTwo = jest.spyOn(MyMathLibrary, "multiplyByTwo");
 
         expect(MyNumber).toEqual(5);
 
         /* ... */
 
-        expect(MyMathLibrary.multiplyByTwo).toHaveBeenCalled();
+        expect(spyOnMultiplyByTwo).toHaveBeenCalled();
         expect(MyNumber).toEqual(10);
     })
 })
 
 
 describe('functional scope - scopes and closures', () => {
-    it('should belong to the function scope', function(){
+    test('should belong to the function scope', () => {
         // declare a variable MyNumber of type number (with an initial value of 9), that should be available in the global scope (outside the MyMathLibrary object)
-        // update the doMath() method code to make the MyNumber value equal 10 / hint: you just need to add one line of code
+        // update the doMath() method code to make the MyNumber value equal 10 / hint: you just need to add one line of code.
         // make sure the MyMathLibrary.doMath() method has been called (with a proper parameter value)
         // make sure the addSix() and reduceFive() functions are NOT available in the global scope
 
-        var MyMathLibrary = {
+        let MyMathLibrary = {
             doMath: function(arg) {
-                MyNumber = addSix(arg);
+                MyNumber = addSix(arg); // WARNING: Do not modify this line!
             }
         }
 
@@ -44,14 +45,14 @@ describe('functional scope - scopes and closures', () => {
         }
 
 
-        spyOn(MyMathLibrary, "doMath").and.callThrough();
+        spyOnDoMath = jest.spyOn(MyMathLibrary, "doMath");
 
         // testing initial value
         expect(MyNumber).toEqual(9);
 
         /* ... */
 
-        expect(MyMathLibrary.doMath).toHaveBeenCalled();
+        expect(spyOnDoMath).toHaveBeenCalled();
 
         // testing result value
         expect(MyNumber).toEqual(10);
@@ -59,13 +60,13 @@ describe('functional scope - scopes and closures', () => {
         expect(typeof reduceFive).toEqual("undefined");
     })
 
-    it('should belong to the function scope - IIFE', function(){
+    test('should belong to the function scope - IIFE', () => {
         // declare a variable MyNumber of type number (with an initial value of 10), that should be available in the global scope (outside the MyMathLibrary object)
         // DO NOT modify the MyMathLibrary.doMath() method code
         // make sure the MyMathLibrary.doMath() method has NOT been called within the global scope
         // call the doSomeMoreMath() function but make sure it is not poluting the global scope (use IIFE)
 
-        var MyMathLibrary = {
+        let MyMathLibrary = {
             doMath: function(arg) {
                 MyNumber = addSix(arg);
             }
@@ -79,11 +80,11 @@ describe('functional scope - scopes and closures', () => {
             MyMathLibrary.doMath(MyNumber)
         }
 
-        spyOn(MyMathLibrary, "doMath").and.callThrough();
+        spyOnDoMath = jest.spyOn(MyMathLibrary, "doMath");
 
         doSomeMoreMath();
 
-        expect(MyMathLibrary.doMath).not.toHaveBeenCalled();
+        expect(spyOnDoMath).not.toHaveBeenCalled();
 
         // testing result value
         expect(MyNumber).toEqual(16);
